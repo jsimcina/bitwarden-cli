bb75703c08338377a0af9cec22f09508e1324d95
Jon <jsimcina@gmail.com>
2024-02-26 15:13:28 -0600

    Test again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+EXPORT BW_CLIENTID=${BW_CLIENTID}
+EXPORT BW_CLIENTSECRET=${BW_CLIENTSECRET}
+EXPORT BW_PASSWORD=${BW_PASSWORD}
 set -e
 
 bw config server ${BW_HOST}
 
-export BW_SESSION=$(bw login --apikey ${BW_CLIENTID} ${BW_CLIENTSECRET})
+export BW_SESSION=$(bw login --apikey)
 
 bw unlock --check
 
